6ee33a2027dc5b8ac8390dd22c2457a9f63d74ea
Tancred423 <thonkred@gmail.com>
2022-05-26 11:31:54 +0200

    ADD color support. CHANGE command execute functions to async. FIX log timestamps.

--- src/core/MyUser.ts
+++ src/core/MyUser.ts
@@ -1,4 +1,4 @@
-import { User } from 'discord.js'
+import { ColorResolvable, User } from 'discord.js'
 import config from '../config.json'
 import { LanguageKeys } from '../Localization/LanguageKeys'
 import { Logger } from '../Logging/Logger'
@@ -24,16 +24,39 @@ export class MyUser {
     try {
       const res = await Database.query(sql)
 
-      if (res.length > 0) {
+      if (typeof res === 'object' && res.hasOwnProperty('language_code')) {
         const keyString = res['language_code'] as string
-
         if (typeof keyString === 'string')
           key = keyString.toUpperCase() as keyof typeof LanguageKeys
       }
     } catch (error: any) {
-      Logger.log(LogTypes.ERROR, error.message, error.code)
+      Logger.log(LogTypes.ERROR, 1653555689, error.message)
     }
 
     return LanguageKeys[key]
+  }
+
+  async getColor(): Promise<ColorResolvable> {
+    return this.getColorCode() as unknown as ColorResolvable
+  }
+
+  async getColorCode(): Promise<string> {
+    const sql = `SELECT color_code
+                 FROM   users
+                 WHERE  user_id=${Database.escape(this.userId)}`
+
+    let color = config.defautColor
+
+    try {
+      const res = await Database.query(sql)
+
+      if (typeof res === 'object' && res.hasOwnProperty('color_code')) {
+        color = res['color_code']
+      }
+    } catch (error: any) {
+      Logger.log(LogTypes.ERROR, 1653555694, error.message)
+    }
+
+    return color
   }
 }
